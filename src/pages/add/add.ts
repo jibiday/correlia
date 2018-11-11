329e862e6d5444ebe8cd452f8af4d460526279e2
Shutov Petr <pmshutov@gmail.com>
2018-11-11 11:35:46 +0300

    hide empty blocks on Add page; disable not ready functional buttons

--- src/pages/add/add.ts
+++ src/pages/add/add.ts
@@ -1,10 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {NavController, ToastController} from 'ionic-angular';
-import * as moment from 'moment';
-import {NoteProvider} from '../../providers/note/note';
-import {PointProvider} from '../../providers/point/point';
-import {ValueProvider} from '../../providers/value/valueProvider';
-import {Note, Point, Value, ValueType} from '../../domain/Symptom';
+import {Component, OnInit} from "@angular/core";
+import {NavController, ToastController} from "ionic-angular";
+import * as moment from "moment";
+import {NoteProvider} from "../../providers/note/note";
+import {PointProvider} from "../../providers/point/point";
+import {ValueProvider} from "../../providers/value/valueProvider";
+import {Note, Point, Value, ValueType} from "../../domain/Symptom";
 
 @Component({
   selector: 'page-add',
@@ -15,6 +15,7 @@ export class AddPage implements OnInit{
   ValueType = ValueType;
   now = moment().format('YYYY-MM-DDTHH:mmZ');
   values: Value[] = [];
+  grouped: any = {};
   selectedDate = moment().format('YYYY-MM-DDTHH:mmZ');
 
   constructor(public navCtrl: NavController,
@@ -35,6 +36,9 @@ export class AddPage implements OnInit{
     this.valueProvider.getAll().then(values => {
       this.values = this.values.concat(values.filter(newVal => !this.values.find(v => v.id === newVal.id)));
       this.values = this.values.filter(deletedVal => values.find(v => v.id === deletedVal.id));
+      this.grouped.intensities = this.values.filter(val => val.type == ValueType.intensity);
+      this.grouped.events = this.values.filter(val => val.type == ValueType.event);
+      this.grouped.intervals = this.values.filter(val => val.type == ValueType.interval);
     })
   }
 
